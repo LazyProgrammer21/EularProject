e93b89a531ea9ebd9ce538cddd0a00272a19e3a5
lazyprogrammer <tamang.subash36@gmail.com>
2023-05-08 20:25:43 +0100

    solution1

--- problem1.js
+++ problem1.js
@@ -1,3 +1,21 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
 
+
+//solution1
+function multiplesOf3and5(number) {
+
+   var sum=0; 
+for (var i=1;i<number;i++){
+   if(i%5==0 || i%3==0){
+    sum+=i
+   }
+}
+return sum
+
+}
+
+console.log(multiplesOf3and5(10));
+
+
+//solution2
